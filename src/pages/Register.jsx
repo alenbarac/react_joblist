b8846db68333a9957869e02409b3c7edc98047a9
Alen <info@alenbarac.com>
2023-06-01 23:43:57 +0200

    register component - base form setup

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,46 @@
+import { useState, useEffect } from 'react'
+import { Logo } from '../components'
+import Wrapper from '../assets/wrappers/RegisterPage'
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
+}
 const Register = () => {
-  return <div>Register</div>
+  const [values, setValues] = useState(initialState)
+
+  const handleChange = (e) => {
+    console.log(e.target)
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log(e.target)
+  }
+
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            name
+          </label>
+          <input
+            type="text"
+            name="name"
+            value={values.name}
+            onChange={handleChange}
+            className="form-input"
+          />
+        </div>
+        <button className="btn btn-block">Submit</button>
+      </form>
+    </Wrapper>
+  )
 }
 
 export default Register
